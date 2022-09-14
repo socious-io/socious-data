7ae6e5c9b44c167f9573fc99af57e16bd877e52e
Lalo Martins <lalo.martins@gmail.com>
2022-09-14 16:16:37 +0300

    reexport translations as `text` prop

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import * as enums from './enums.js'
-import * as schemas from './schemas.js'
+import * as enums from './enums.js';
+import * as schemas from './schemas.js';
+import * as text_en from './translations/en.json' assert {type: "json"};
 
 export default {
   ...enums,
-  ...schemas
-}
+  ...schemas,
+  text: {
+    en: text_en,
+  },
+};
