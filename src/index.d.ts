b4fa35e3471d16fb050a21c9fed9807ff5a80949
Jeyem <me@e-mahmoudi.me>
2022-09-16 16:51:10 +0430

    skip types for now

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,9 @@
 import * as schema from './schemas';
 import * as enums from './enums';
-import * as types from './types';
 
 export default {
   ...schema,
   ...enums,
-  ...types,
   text: {
     en: Object,
   },
