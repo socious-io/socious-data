5e5757c0350e7b5785fd00ae8ad75c71200550a2
Jeyem <me@e-mahmoudi.me>
2022-09-16 16:48:08 +0430

    full types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,12 @@
-import Joi from "joi";
 import * as schema from './schemas'
+import * as enums from './enums'
+import * as types from './types'
 
 export default {
-  ...schema
+  ...schema,
+  ...enums,
+  ...types,
+  text: {
+    en: Object
+  }
 }
