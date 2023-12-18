4d6ec8f0c937837fa9e246fe281e51474b97201f
jeyem <me@e-mahmoudi.me>
2023-12-18 20:27:26 +0330

    names validator regex

--- src/validators.js
+++ src/validators.js
@@ -134,10 +134,11 @@ export const usernamePattern =
   /^(?=.{6,24}$)(?![_.])(?!.*[_.]{2})[a-z0-9._-]+$/;
 
 export const languagePattern = /^[a-z][a-z](-[a-z][a-z])?$/;
+export const namesPattern = /^\p{L}+(([\p{M}'\-. ]\p{L})?\p{L}*)*$/;
 
 export const UpdateProfileSchema = Joi.object({
-  first_name: Joi.string().required(),
-  last_name: Joi.string().required(),
+  first_name: Joi.string().regex(namesPattern).required(),
+  last_name: Joi.string().regex(namesPattern).required(),
   username: Joi.string().regex(usernamePattern).required(),
   bio: Joi.string(),
   mission: Joi.string(),
